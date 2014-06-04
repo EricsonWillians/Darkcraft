82e79a96c5b2cb1e2c12a672a3d1186f62310972
Ericson Willians Rocha Pires. <ericsonwrp@gmail.com>
2014-06-03 23:47:23 -0300

    Update error.py

--- error.py
+++ error.py
@@ -1,7 +1,7 @@
 """
 ====================================================================
 
-PYTHUN ENGINE 2.1 - Stable Release
+PYTHUN ENGINE 2.1 - Stable Release.
 Copyright (C) <2014>  <Ericson Willians.>
 
 This program is free software: you can redistribute it and/or modify
